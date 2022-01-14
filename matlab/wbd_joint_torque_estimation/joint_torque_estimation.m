9db8822308c5533a1565892b2bc0566cf846b298
Ines <ines.sorrentino10@gmail.com>
2022-01-14 14:59:22 +0100

    Implement joint torque estimation using iDynTree

--- matlab/wbd_joint_torque_estimation/joint_torque_estimation.m
+++ matlab/wbd_joint_torque_estimation/joint_torque_estimation.m
@@ -1,5 +1,16 @@
+addpath(genpath('..'));
+
 modelPath = '../urdf/';
 modelName = 'iCub3_right_leg.urdf';
+
+num_datasets = 1;
+load_dataset_bool = true;
+leg = 'right';
+joint = 21:26;
+load_dataset;
+
+%% Prepare data
+[~, joint_acc] = estimateVelAccFromPos(joint_pos);
 
 %% Load the estimator
 estimator = iDynTree.ExtWrenchesAndJointTorquesEstimator();
@@ -13,50 +24,67 @@ grav_idyn = iDynTree.Vector3();
 grav = [0.0;0.0;-9.81];
 grav_idyn.fromMatlab(grav);
 
-% Get joint information. for the time being we can just assume that
-% all the joints position, velocity and acceleration are zero.
-% Warning!! iDynTree takes in input **radians** based units,
-% while the iCub port stream **degrees** based units.
+% Get joint information
 dofs = estimator.model().getNrOfDOFs();
-
-% >>>>>>>>>>>>>>>> Questo va in un for loop
-qj = zeros(dofs,1);
-dqj = zeros(dofs,1);
-ddqj = zeros(dofs,1);
 
 qj_idyn   = iDynTree.JointPosDoubleArray(dofs);
 dqj_idyn  = iDynTree.JointDOFsDoubleArray(dofs);
 ddqj_idyn = iDynTree.JointDOFsDoubleArray(dofs);
 
+qj = joint_pos(:,1);
+dqj = joint_vel(:,1);
+ddqj = joint_acc(:,1);
+
 qj_idyn.fromMatlab(qj);
 dqj_idyn.fromMatlab(dqj);
 ddqj_idyn.fromMatlab(ddqj);
 
-r_sole_index = estimator.model().getFrameIndex('r_sole');
-estimator.updateKinematicsFromFixedBase(qj_idyn,dqj_idyn,ddqj_idyn,r_sole_index,grav_idyn);
-
+% Set the kinematics information in the estimator
+root_link_index = estimator.model().getFrameIndex('root_link');
+estimator.updateKinematicsFromFixedBase(qj_idyn,dqj_idyn,ddqj_idyn,root_link_index,grav_idyn);
 
 %% Specify unknown wrenches
-
 % We need to set the location of the unknown wrench. We express the unknown
-% wrench at the origin of the r_sole frame
+% wrench at the origin of the l_sole frame
 unknownWrench = iDynTree.UnknownWrenchContact();
 unknownWrench.unknownType = iDynTree.FULL_WRENCH;
 
-% the position is the origin, so the conctact point wrt to r_sole is zero
-unknownWrench.contactPoint.zero();
+%% Set contacts
+framesNames={'r_foot_rear','r_foot_front','root_link'};
+for frame=1:length(framesNames) 
+    fullBodyUnknownsExtWrenchEst.addNewUnknownFullWrenchInFrameOrigin(estimator.model(),estimator.model().getFrameIndex(framesNames{frame}));
+end
 
-% The fullBodyUnknowns is a class storing all the unknown external wrenches
-% acting on a class
-fullBodyUnknowns = iDynTree.LinkUnknownWrenchContacts(estimator.model());
-fullBodyUnknowns.clear();
-
-fullBodyUnknowns.addNewContactInFrame(estimator.model(),r_sole_index,unknownWrench);
-
-% Print the unknowns to make sure that everything is properly working
-fullBodyUnknowns.toString(estimator.model())
+% The estimated external wrenches
+estContactForcesExtWrenchesEst = iDynTree.LinkContactWrenches(estimator.model());
 
 
+% The estimated joint torques
+estJointTorquesExtWrenchesEst = iDynTree.JointDOFsDoubleArray(dofs);
+
+
+for ftIndex = 0:(nrOfFTSensors-1)
+    sens = estimator.sensors().getSensor(iDynTree.SIX_AXIS_FORCE_TORQUE,ftIndex).getName();
+    matchup(ftIndex+1) = find(strcmp(sensorNames,sens ));
+end
+
+
+for t=1:size(joint_pos,1)
+    qj=joint_pos(:,t);
+    dqj=dqj_all(:,t);
+    ddqj=ddqj_all(:,t);
+    
+    
+    qj_idyn.fromMatlab(qj);
+    dqj_idyn.fromMatlab(dqj);
+    ddqj_idyn.fromMatlab(ddqj);
+    
+    if(length(contactFrameName)>1)
+        contact_index = estimator.model().getFrameIndex(char(contactFrameName(t)));
+    end
+    
+    ok = estimator.updateKinematicsFromFixedBase(qj_idyn,dqj_idyn,ddqj_idyn,contact_index,grav_idyn);
+end
 
 
 
