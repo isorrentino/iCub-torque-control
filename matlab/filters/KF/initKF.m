c48ee28eb339ffb282e6d820e866cc00b842b8ac
Ines <ines.sorrentino10@gmail.com>
2022-01-17 00:15:25 +0100

    Implement second order pure kinematic KF

---
+++ matlab/filters/KF/initKF.m
@@ -0,0 +1,28 @@
+function kf = initKF(x0, stateSize, dT)
+% The function kf = initKF(x0, stateSize) initializes the variables of the
+% kalman filter.
+
+% Model characteristics
+% X(k) = A * X(k-1) + v ---> A: State transition matrix, v=N(0,Q) : Process noise
+% z(k) = H * X(k)   + w ---> H: Output matrix          , w=N(0,R) : Observation noise
+
+% Init filter
+
+% Set covariances
+kf.Q = diag([1e-5; 0.005; 0.1]);  % Process noise
+kf.R = 1e-7; %1e-5;  % Measurement noise
+
+kf.A = eye(stateSize);
+for ii = 1 : stateSize-1
+  for jj = ii+1 : stateSize
+    kf.A(ii,jj) = dT^(jj-ii)/factorial(jj-ii);
+  end
+end
+
+kf.H = [1  zeros(1,stateSize-1)];
+
+kf.x = x0;
+
+kf.Px = kf.H \ (kf.R / kf.H');
+
+end
