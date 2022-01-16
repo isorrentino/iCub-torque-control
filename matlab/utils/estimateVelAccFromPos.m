8ada85cd0d911810f341cda1488d29f51bc72205
Ines <ines.sorrentino10@gmail.com>
2022-01-17 00:14:37 +0100

    Add some utilities like joint velocity and acceleration estimation

---
+++ matlab/utils/estimateVelAccFromPos.m
@@ -0,0 +1,32 @@
+function [vel,acc] = estimateVelAccFromPos(s_dataset, dT)
+
+% Expected pos size samples x joints
+if size(s_dataset,1) < size(s_dataset,2)
+    pos = s_dataset';
+else
+    pos = s_dataset;
+end
+
+% Estimate acceleration
+stateSize = 3;
+sdot_dataset = zeros(size(pos));
+sddot_dataset = zeros(size(pos));
+
+for j = 1 : 6
+x0 = [pos(1,j); zeros(stateSize-1,1)];
+kf = initKF(x0, stateSize, dT);
+% Run filter for entire traj
+xh = zeros(stateSize, size(pos,1));
+for sample = 1 : size(pos,1)
+  kf.z = pos(sample,j);
+  kf = kalmanfilter(kf);
+  xh(:,sample) = kf.x;
+end
+sdot_dataset(:,j) = xh(2,:)';
+sddot_dataset(:,j) = xh(3,:)';
+end
+
+vel = sdot_dataset';
+acc = sddot_dataset';
+  
+end
