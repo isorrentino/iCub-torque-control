8f330cfd5a4e7a1513d360b05e9599506b4d1a9e
Ines <ines.sorrentino10@gmail.com>
2022-03-17 16:03:56 +0100

    Change how to read data from dataset in kf tuning

--- matlab/filters/kf/kfTuning.m
+++ matlab/filters/kf/kfTuning.m
@@ -1,15 +1,11 @@
 % close all;
 
-joint = 27;
-
 fs = 100;
 
-joint_pos = reshape(robot_logger_device.walking.wbd.kf.position.data,32,[]);
-joint_vel = reshape(robot_logger_device.walking.wbd.kf.velocity.data,32,[]);
-joint_acc = reshape(robot_logger_device.walking.wbd.kf.acceleration.data,32,[]);
-joint_pos_kf = reshape(robot_logger_device.walking.wbd.kf.positionKF.data,32,[]);
-joint_vel_kf = reshape(robot_logger_device.walking.wbd.kf.velocityKF.data,32,[]);
-joint_acc_kf = reshape(robot_logger_device.walking.wbd.kf.accelerationKF.data,32,[]);
+n_joints = 26;
+
+joint_pos = reshape(robot_logger_device.joints_state.positions.data,n_joints,[]);
+joint_vel = reshape(robot_logger_device.joints_state.velocities.data,n_joints,[]);
 
 % Number of joints
 n = size(joint_pos,1);
@@ -32,11 +28,11 @@ ddx_numeric = [zeros(n,1), (diff(joint_vel')*fs)'];
 for i = 1 : n
     d = designfilt('lowpassiir','FilterOrder',order, ...
         'HalfPowerFrequency',fx,'DesignMethod','butter');
-
+    
     % Filter signal using Zero-phase digital filtering filtfilt
     
-    dx_filt(i,:) = filtfilt(d,joint_vel(i,:)'); 
-
+    dx_filt(i,:) = filtfilt(d,joint_vel(i,:)');
+    
     % Filter signal using Zero-phase digital filtering filtfilt
     ddx_filt(i,:) = filtfilt(d,ddx_numeric(i,:)');
 end
@@ -59,23 +55,25 @@ end
 x0 = [joint_pos(:,1); zeros(2*n,1)];
 
 
-q = [repmat(1e-3,n,1); repmat(1e-3,n,1); repmat(1e1,n,1)];
-r = repmat(1e-3,n,1);
+q = [repmat(1e-2,n,1); repmat(1e-1,n,1); repmat(1e1,n,1)];
+r = repmat(1e-4,n,1);
 
 kf = initKFNullJerk(x0, q, r, 1/fs);
 
 % Filter signal
 for sample = 1 : size(joint_pos,2)
-  kf.z = joint_pos(:,sample);
-  kf = kalmanfilter(kf);
-  xh(:,sample) = kf.x;
+    kf.z = joint_pos(:,sample);
+    kf = kalmanfilter(kf);
+    xh(:,sample) = kf.x;
 end
 
 dx_kf = xh(n+1:2*n,:);
 ddx_kf = xh(2*n+1:end,:);
 
-% Plot fitlered signal
-% Plot fitlered signal
+
+joint = 7;
+
+
 figure
 subplot(2,1,1)
 hold on
@@ -100,18 +98,16 @@ movegui('northwest')
 figure,
 subplot(2,1,1)
 plot(dx_kf(joint,:)')
-hold on
-plot(joint_vel_kf(joint,:)')
+% hold on
+% plot(joint_vel_kf(joint,:)')
 legend('offline','online')
 title('Vel kf offline - online')
 
 subplot(2,1,2)
 plot(ddx_kf(joint,:)')
-hold on
-plot(joint_acc_kf(joint,:)')
+% hold on
+% plot(joint_acc_kf(joint,:)')
 legend('offline','online')
 title('Acc kf offline - online')
-
-
 
 
