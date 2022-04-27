01bd80c108175f75a3a8f82813b0df3fb703a883
Ines <ines.sorrentino10@gmail.com>
2022-04-27 18:06:10 +0200

    Move TSID code

---
+++ code/TSID/build/src/iCubFixedBaseTSID/CMakeFiles/iCubFixedBaseTSID.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/isorrentino/dev/iCub-torque-control/src/iCubFixedBaseTSID/src/Main.cpp" "/home/isorrentino/dev/iCub-torque-control/build/src/iCubFixedBaseTSID/CMakeFiles/iCubFixedBaseTSID.dir/src/Main.cpp.o"
+  "/home/isorrentino/dev/iCub-torque-control/src/iCubFixedBaseTSID/src/Module.cpp" "/home/isorrentino/dev/iCub-torque-control/build/src/iCubFixedBaseTSID/CMakeFiles/iCubFixedBaseTSID.dir/src/Module.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SPDLOG_COMPILED_LIB"
+  "USING_DEPRECATED_UPPERCASE_YARP_OS_TARGET"
+  "_USE_MATH_DEFINES"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/iCubFixedBaseTSID/include"
+  "/home/isorrentino/dev/robotology-superbuild/build/install/include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
